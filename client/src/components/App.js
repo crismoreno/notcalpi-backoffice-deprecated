b9e87dcb6c55765f3283de0680e2bbf4d403d8d5
Cristina Moreno Medran <cmoreno@redpoints.com>
2020-06-10 19:56:55 +0200

    fetching data from backend in this commit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const App = () => (
-		<h1>Hello from React!</h1>
-);
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      const response = await axios("http://localhost:5000/api");
+      setData(response.data);
+    }
+    fetchData();
+  }, []); // Or [] if effect doesn't need props or state
+
+  return (
+    <div>
+      <h1>Hello from React!</h1>
+      {/* <pre>{data}</pre> */}
+    </div>
+  );
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import state from './reducers';
-import App from './components/App';
+import React from "react";
+import { render } from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import state from "./reducers";
+import App from "./components/App";
 
-const store = createStore(
-    state
-);
+require("@babel/polyfill");
+
+const store = createStore(state);
 
 render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('app')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
 );
