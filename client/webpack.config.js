c7fea3b175989d85bb4bbf4eaca95c14c3992ac8
Cristina Moreno Medran <cmoreno@redpoints.com>
2020-06-10 19:34:16 +0200

    workign fine here full setup

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,25 +1,30 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'docs'),
-        filename: 'bundle.js'
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + "../../server/public/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: __dirname + "../../server/public/",
+    compress: true,
+    port: 9000,
+    proxy: {
+      "/api": "http://localhost:5000/",
     },
-    module: {
-        rules: [
-            { 
-                test: /\.jsx?$/, 
-                loader: 'babel-loader', 
-                exclude: /node_modules/ 
-            },
-            {
-                test: /\.css$/,
-                use: [ 'style-loader', 'css-loader' ]
-            }
-        ]
-    },
-    devServer: {
-        contentBase: './docs'
-    }
+  },
 };
