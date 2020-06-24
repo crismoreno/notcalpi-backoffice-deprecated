335d04978bd9ec60c98be49d01fea445d9e7e734
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-06-24 19:51:29 +0200

    mapping projects in app.js view

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,11 @@
+import projects from './projects/projects';
 import { combineReducers } from 'redux';
-import projects from './projects';
 
-export default combineReducers({
+const reducers = combineReducers({
   projects,
 });
+
+export default reducers;
+
+export const getProjects = (state) => state.projects.projects;
+// console.log(state.projects.projects, 'projects');
--- client/src/views/App.js
+++ client/src/views/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import fetchProjects from '../helpers/getProjects';
 import { connect } from 'react-redux';
 
+import { getProjects } from '../reducers/index';
+
 // import {
 //   EditOutlined,
 //   EllipsisOutlined,
@@ -10,54 +12,22 @@ import { connect } from 'react-redux';
 // import { Card, Avatar } from 'antd';
 // const { Meta } = Card;
 
-const App = ({ dispatch }) => {
+const App = ({ dispatch, projects }) => {
   useEffect(() => {
     dispatch(fetchProjects());
-  }, []);
-
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     // You can await here
-  //     const response = await axios('http://localhost:5000/api');
-  //     setData(response.data);
-  //   }
-  //   fetchData();
-  // }, []); // Or [] if effect doesn't need props or state
+  }, [projects]);
 
   return (
     <div>
-      <h1>Hello from React</h1>
-      {/* <Card
-        style={{ width: 300 }}
-        cover={
-          <img
-            alt="example"
-            src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-          />
-        }
-        actions={[
-          <SettingOutlined key="setting" />,
-          <EditOutlined key="edit" />,
-          <EllipsisOutlined key="ellipsis" />,
-        ]}
-      >
-        <Meta
-          avatar={
-            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-          }
-          title="Card title"
-          description="This is the description"
-        />
-      </Card>
-      , */}
+      {projects.map(({ project }) => (
+        <h1>Hello!</h1>
+      ))}
     </div>
   );
 };
 
 const mapStateToProps = (state) => ({
-  projects: state.projects,
-  loading: state.loading,
-  error: state.error,
+  projects: getProjects(state),
 });
 
 export default connect(mapStateToProps)(App);
