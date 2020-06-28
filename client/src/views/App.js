03c66eadade75fc571ff0fe67955602ea4d2b1d8
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-06-28 21:10:37 +0200

    showing projects here already

--- client/src/views/App.js
+++ client/src/views/App.js
@@ -4,13 +4,13 @@ import { connect } from 'react-redux';
 
 import { getProjects } from '../reducers/index';
 
-// import {
-//   EditOutlined,
-//   EllipsisOutlined,
-//   SettingOutlined,
-// } from '@ant-design/icons';
-// import { Card, Avatar } from 'antd';
-// const { Meta } = Card;
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
+import { Card, Avatar } from 'antd';
+const { Meta } = Card;
 
 const App = ({ dispatch, projects }) => {
   useEffect(() => {
@@ -19,8 +19,30 @@ const App = ({ dispatch, projects }) => {
 
   return (
     <div>
-      {projects.map(({ project }) => (
-        <h1>Hello!</h1>
+      {projects.map((project, index) => (
+        // <h1 key={index}>{project.title}</h1>
+        <Card
+          style={{ width: 300 }}
+          cover={
+            <img
+              alt="example"
+              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+            />
+          }
+          actions={[
+            <SettingOutlined key="setting" />,
+            <EditOutlined key="edit" />,
+            <EllipsisOutlined key="ellipsis" />,
+          ]}
+        >
+          <Meta
+            avatar={
+              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            }
+            title={project.title}
+            description={project.customer}
+          />
+        </Card>
       ))}
     </div>
   );
