bded7e1b2f55cd826379a09e1529f4a2ba7cbfd8
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-07-08 19:28:14 +0200

    more styling here

--- client/src/views/App.jsx
+++ client/src/views/App.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import useDeepCompareEffect from 'use-deep-compare-effect';
 import fetchProjects from '../helpers/getProjects';
 import { connect } from 'react-redux';
@@ -16,8 +17,8 @@ import {
 } from '@ant-design/icons';
 import { Layout, Menu } from 'antd';
 
+const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
-const { Header, Content, Sider } = Layout;
 
 const App = ({ dispatch, projects }) => {
   useDeepCompareEffect(() => {
@@ -26,55 +27,64 @@ const App = ({ dispatch, projects }) => {
     }
   }, [projects]);
 
+  const [collapsed, setCollapsed] = useState(false);
+  const onCollapse = () => {
+    if (collapsed === false) {
+      setCollapsed(true);
+    }
+    if (collapsed === true) {
+      setCollapsed(false);
+    }
+  };
+
   return (
-    <Layout>
-      <Header className="header">
-        <div className="logo">
-          <img height="30" src={require('../assets/img/white-logo.svg')} />
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+        <div className="logo" style={{ display: 'flex' }}>
+          <img
+            style={{ padding: '10px', height: '60px', margin: '0 auto' }}
+            height="30"
+            src={require('../assets/img/mini-logo.svg')}
+          />
         </div>
-        <Menu mode="horizontal"></Menu>
-      </Header>
-      <Layout>
-        <Sider width={200} className="site-layout-background">
-          <Menu
-            mode="inline"
-            defaultSelectedKeys={['1']}
-            defaultOpenKeys={['sub1']}
-            style={{ height: '100%', borderRight: 0 }}
-          >
-            <Menu.Item key="1" icon={<FolderOpenOutlined />}>
-              Projects
-            </Menu.Item>
-            <SubMenu key="sub1" icon={<FlagOutlined />} title="Categories">
-              <Menu.Item key="2">Tags</Menu.Item>
-              <Menu.Item key="3">Coding Languages</Menu.Item>
-              <Menu.Item key="4">Made Ats</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="2" icon={<MailOutlined />}>
-              Contact Forms
-            </Menu.Item>
-            <Menu.Item key="3" icon={<UserOutlined />}>
-              Users
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout style={{ padding: '0 24px 24px' }}>
-          <Content
-            className="site-layout-background"
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-            }}
-          >
-            <Button type="primary">Add new project</Button>
-            <div className="project-cards-container">
-              {projects.map((project, index) => (
-                <ProjectCard project={project} key={index} />
-              ))}
-            </div>
-          </Content>
-        </Layout>
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1" icon={<FolderOpenOutlined />}>
+            Projects
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<FlagOutlined />} title="Categories">
+            <Menu.Item key="2">Tags</Menu.Item>
+            <Menu.Item key="3">Coding Languages</Menu.Item>
+            <Menu.Item key="4">Made Ats</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="2" icon={<MailOutlined />}>
+            Contact Forms
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UserOutlined />}>
+            Users
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Content
+          style={{
+            margin: '0 16px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-end',
+          }}
+        >
+          <Button type="primary" style={{ marginTop: '15px' }}>
+            Add new project
+          </Button>
+          <div className="project-cards-container">
+            {projects.map((project, index) => (
+              <ProjectCard project={project} key={index} />
+            ))}
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
       </Layout>
     </Layout>
   );
