3208cbe5e17b04c59f6ab202a5286d66f51a8acd
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-07-11 17:11:50 +0200

    got the login form here

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Welcome from './views/Welcome.jsx';
 import Category from './views/Category.jsx';
 import ContactForms from './views/ContactForms.jsx';
 import Users from './views/Users.jsx';
+import Login from './views/Login.jsx';
 
 import {
   UserOutlined,
@@ -13,8 +14,9 @@ import {
   MailOutlined,
   SmileOutlined,
   DesktopOutlined,
+  LogoutOutlined,
 } from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Button } from 'antd';
 
 const { Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
@@ -28,75 +30,89 @@ const App = (store) => {
 
   return (
     <Router>
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
-          <div className="logo">
-            <Link to="/" style={{ display: 'flex' }}>
-              <img
-                style={{ padding: '10px', height: '60px', margin: '0 auto' }}
-                height="30"
-                src={require('./assets/img/mini-logo.svg')}
-              />
-            </Link>
-          </div>
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1" icon={<SmileOutlined />}>
-              <Link to="/">Welcome</Link>
-            </Menu.Item>
-            <Menu.Item key="2" icon={<FolderOpenOutlined />}>
-              <Link to="/projects">Projects</Link>
-            </Menu.Item>
-            <SubMenu key="sub1" icon={<FlagOutlined />} title="Categories">
-              <Menu.Item key="3">
-                <Link to="/category/tags">Tags</Link>
+      <Route path="/" exact component={Login} />
+      <Route path="/admin">
+        <Layout style={{ minHeight: '100vh' }}>
+          <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
+            <div className="logo">
+              <Link to="/admin" style={{ display: 'flex' }}>
+                <img
+                  style={{ padding: '10px', height: '60px', margin: '0 auto' }}
+                  height="30"
+                  src={require('./assets/img/mini-logo.svg')}
+                />
+              </Link>
+            </div>
+            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+              <Menu.Item key="1" icon={<SmileOutlined />}>
+                <Link to="/admin">Welcome</Link>
               </Menu.Item>
-              <Menu.Item key="4">
-                <Link to="/category/codingLangs">Coding Languages</Link>
+              <Menu.Item key="2" icon={<FolderOpenOutlined />}>
+                <Link to="/admin/projects">Projects</Link>
               </Menu.Item>
-              <Menu.Item key="5">
-                <Link to="/category/madeAts">Made Ats</Link>
+              <SubMenu key="sub1" icon={<FlagOutlined />} title="Categories">
+                <Menu.Item key="3">
+                  <Link to="/admin/category/tags">Tags</Link>
+                </Menu.Item>
+                <Menu.Item key="4">
+                  <Link to="/admin/category/codingLangs">Coding Languages</Link>
+                </Menu.Item>
+                <Menu.Item key="5">
+                  <Link to="/admin/category/madeAts">Made Ats</Link>
+                </Menu.Item>
+              </SubMenu>
+              <Menu.Item key="6" icon={<MailOutlined />}>
+                <Link to="/admin/contactForms">Contact Forms</Link>
               </Menu.Item>
-            </SubMenu>
-            <Menu.Item key="6" icon={<MailOutlined />}>
-              <Link to="/contactForms">Contact Forms</Link>
-            </Menu.Item>
-            <Menu.Item key="7" icon={<UserOutlined />}>
-              <Link to="/users">Users</Link>
-            </Menu.Item>
-            <Menu.Item key="8" icon={<DesktopOutlined />}>
-              <a target="_blank" href="https://www.cristinamoreno.dev/">
-                Visit FrontOffice
-              </a>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout className="site-layout">
-          <Content
-            style={{
-              margin: '0 16px',
-            }}
-          >
-            <Switch>
-              <Route path="/" exact component={Welcome} />
-              <Route path="/projects">
-                <Projects store={store} />
-              </Route>
-              <Route
-                path="/category/:cat"
-                render={({ match }) => <Category match={match} store={store} />}
-              />
-              <Route
-                path="/contactForms"
-                render={() => <ContactForms store={store} />}
-              />
-              <Route path="/users" render={() => <Users store={store} />} />
-            </Switch>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>
-            Made with love by Cristina Moreno ❤️
-          </Footer>
+              <Menu.Item key="7" icon={<UserOutlined />}>
+                <Link to="/admin/users">Users</Link>
+              </Menu.Item>
+              <Menu.Item key="8" icon={<DesktopOutlined />}>
+                <a target="_blank" href="https://www.cristinamoreno.dev/">
+                  Visit FrontOffice
+                </a>
+              </Menu.Item>
+              <Menu.Item key="9" icon={<LogoutOutlined />}>
+                <Link to="/">Logout</Link>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout className="site-layout">
+            <Content
+              style={{
+                margin: '0 16px',
+              }}
+            >
+              <Switch>
+                <Route path="/admin" exact component={Welcome} />
+                <Route path="/admin/projects" exact>
+                  <Projects store={store} />
+                </Route>
+                <Route
+                  path="/admin/category/:cat"
+                  exact
+                  render={({ match }) => (
+                    <Category match={match} store={store} />
+                  )}
+                />
+                <Route
+                  path="/admin/contactForms"
+                  exact
+                  render={() => <ContactForms store={store} />}
+                />
+                <Route
+                  path="/admin/users"
+                  exact
+                  render={() => <Users store={store} />}
+                />
+              </Switch>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+              Made with love by Cristina Moreno ❤️
+            </Footer>
+          </Layout>
         </Layout>
-      </Layout>
+      </Route>
     </Router>
   );
 };
