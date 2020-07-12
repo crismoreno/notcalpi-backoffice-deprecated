a26f1b903d06dc933886bfc6c52de3e21f7a9e69
Cristina Moreno Medran <moreno.medran@gmail.com>
2020-07-12 16:18:13 +0200

    fix sending bearer in protected requests

--- client/src/helpers/authHeader.js
+++ client/src/helpers/authHeader.js
@@ -3,7 +3,7 @@ export function authHeader() {
   let user = JSON.parse(localStorage.getItem('user'));
 
   if (user) {
-    return { Authorization: 'Bearer ' + user };
+    return { Authorization: 'Bearer ' + user.token };
   } else {
     return {};
   }
