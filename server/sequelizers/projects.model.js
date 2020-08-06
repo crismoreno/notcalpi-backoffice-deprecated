e04e078aec8d1df2adc151b3b65d852f029e1114
Cristina Moreno <moreno.medran@gmail.com>
2020-08-06 20:03:37 +0200

    adding sequelizer here

--- server/sequelizers/projects.model.js
+++ server/sequelizers/projects.model.js
@@ -34,6 +34,9 @@ module.exports = (sequelize, Sequelize) => {
     link_to_repo: {
       type: Sequelize.STRING,
     },
+    link_to_post: {
+      type: Sequelize.STRING,
+    },
     link_to_download: {
       type: Sequelize.STRING,
     },
