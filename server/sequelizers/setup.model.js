f9718aac7749da2cd3e7592367b4d60f7c0a63a5
Cristina Moreno <moreno.medran@gmail.com>
2020-11-14 01:39:57 +0100

    new sequelizer

--- server/sequelizers/setup.model.js
+++ server/sequelizers/setup.model.js
@@ -8,10 +8,64 @@ module.exports = (sequelize, Sequelize) => {
     },
 		limit_featured: {
 			type: Sequelize.INTEGER,
+			allowNull: false,
 		},
 		limit_related: {
 			type: Sequelize.INTEGER,
-		}
-	}, {freezeTableName: true, timestamps: false});
+			allowNull: false,
+		},
+		currently_doing: {
+			type: Sequelize.TEXT,
+      allowNull: false,
+		},
+		cv_share_link: {
+			type: Sequelize.TEXT,
+      allowNull: false,
+		},
+		linkedin_link: {
+			type: Sequelize.TEXT,
+      allowNull: false,
+		},
+		medium_link: {
+			type: Sequelize.TEXT,
+      allowNull: false,
+		},
+		github_link: {
+			type: Sequelize.TEXT,
+      allowNull: false,
+		},
+		instagram_link: {
+			type: Sequelize.TEXT,
+      allowNull: false,
+		},
+		calendly_uri: {
+			type: Sequelize.TEXT,
+      allowNull: false,
+		},
+		phone_num: {
+			type: Sequelize.INTEGER,
+      allowNull: false,
+		},
+		email: {
+			type: Sequelize.TEXT,
+      allowNull: false,
+		},
+		heroku_frontOffice: {
+			type: Sequelize.TEXT,
+      allowNull: false,
+		},
+		heroku_Backoffice: {
+			type: Sequelize.TEXT,
+      allowNull: false,
+		},
+		github_backOffice: {
+			type: Sequelize.TEXT,
+      allowNull: false,
+		},
+		github_frontOffice: {
+			type: Sequelize.TEXT,
+      allowNull: false,
+		},
+	}, {freezeTableName: true});
 	return Setup;
 };
